6c31b1189c347e6af14175961e5140ae2dc11086
YONDU\rasistores <rasistores>
2023-02-22 22:22:57 +0800

    course-project: Menu project DONE

--- react-projects-master/react-projects-master/05-menu/setup/src/App.js
+++ react-projects-master/react-projects-master/05-menu/setup/src/App.js
@@ -4,7 +4,29 @@ import Categories from './Categories';
 import items from './data';
 
 function App() {
-  return <h2>menu project setup</h2>;
+
+  const categories = ['all', ...new Set(items.map((item) => {
+    return item.category;
+ }))]
+
+ const [selectedTab, setSelectedTab ] = useState(categories[0]);
+
+  const setTab = (category) => {
+    setSelectedTab(category);
+  }
+
+  return(
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categories = { categories } setTab = { setTab } />
+        <Menu menu = { items } selectedCategory = { selectedTab }/>
+      </section>
+    </main>
+  );
 }
 
 export default App;
--- react-projects-master/react-projects-master/05-menu/setup/src/Categories.js
+++ react-projects-master/react-projects-master/05-menu/setup/src/Categories.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
-const Categories = () => {
-  return <h2>categories component</h2>;
-};
+const Categories = ({categories, setTab }) => {
+
+  const getCategory = (category) => {
+    setTab(category);
+  }
+  
+  const categoriesList = categories.map((category, index) =>{
+   return( <button key={index} className="filter-btn" onClick={ () => getCategory(category) }>{category}</button>)
+  })
+ return (
+    <div className="btn-container">
+     {categoriesList}
+    </div>
+  );
+}
 
 export default Categories;
