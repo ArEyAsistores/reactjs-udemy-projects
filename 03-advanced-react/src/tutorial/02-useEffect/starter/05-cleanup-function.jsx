9151b8b43e6946cacdc5febfb314a0603134647c
YONDU\rasistores <rasistores>
2023-02-22 14:17:59 +0800

    advance-topic-section: conditional rendering and shot circuit operator

--- 03-advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ 03-advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const CleanupFunction = () => {
+
+  const [ toggle, setToggle ] = useState(false);
+  console.log(toggle);
+
   return (
-    <div>CleanupFunction</div>
+    <div> CleanupFunction
+      <div>
+        <button type='button' className='btn' onClick={() => setToggle(!toggle)}> Toggle </button>
+        {toggle && <RandomComponent/>}
+      </div>
+    </div>
   )
+}
+
+const RandomComponent = () => {
+  // useEffect(() => {
+  //   console.log('hello');
+
+  //   const intId = setInterval(() => {
+  //     console.log('Here');
+  //   }, 2000);
+
+  //   return () => {
+  //     clearInterval(intId);
+  //   };
+  // }, []);
+
+  useEffect(() => {
+    const sumFunc = () => {
+      console.log('Heelo ');
+    }
+    window.addEventListener('scroll', sumFunc);
+    return () => {
+      window.removeEventListener('scroll', sumFunc);
+    }
+
+  },[])
+   
+  return(<h1 className='h1'> HELLO THERE! </h1>);
 }
 
 export default CleanupFunction
--- 03-advanced-react/src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
+++ 03-advanced-react/src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
@@ -1,5 +1,36 @@
-const UserChallenge = () => {
-  return <h2>user challenge</h2>;
-};
+import React, { useState } from 'react'
 
-export default UserChallenge;
+const UserChallenge = () => {
+  const [ user, setUser ] = useState(null);
+
+  const userLogin = (userName) => {
+    setUser({name: 'ArEy'});
+    console.log(`Hello There, welcome ${userName}`)
+  }
+  const userLogout = () => {
+    setUser(null);
+    console.log(`Please Login`)
+  }
+
+  return (
+    <div> UserChallenge 
+        {
+          user ? 
+          <>
+          <h1>Hello There, welcome {user.name}</h1> 
+          <button type='button' className='btn' onClick={userLogout}> Login </button>
+          </>
+        : 
+        <>
+        <h1>Please Login</h1> 
+        <button type='button' className='btn' onClick={userLogin}> Logout </button>
+        </>
+        }
+      <div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default UserChallenge
