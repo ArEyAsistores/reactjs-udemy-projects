50fb8d7fedd7365130b8705a20c57d14f6cd2e0d
YONDU\rasistores <rasistores>
2023-02-28 16:30:52 +0800

    comfy-sloth-project:DONE

--- react-course-comfy-sloth-starter-main/src/components/Footer.js
+++ react-course-comfy-sloth-starter-main/src/components/Footer.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import styled from 'styled-components'
 const Footer = () => {
-  return <h4>footer</h4>
+  return (
+    <Container>
+      <h5>
+        &copy; {new Date().getFullYear()}
+        <span> ComfySloth </span>
+      </h5>
+      <h5>All rights reserved</h5>
+    </Container>
+  )
 }
 
-const Wrapper = styled.footer`
+const Container = styled.footer`
   height: 5rem;
   display: flex;
   flex-direction: column;
--- react-course-comfy-sloth-starter-main/src/components/Product.js
+++ react-course-comfy-sloth-starter-main/src/components/Product.js
@@ -3,11 +3,22 @@ import styled from 'styled-components'
 import { formatPrice } from '../utils/helpers'
 import { FaSearch } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
-
-const Product = () => {
-  return <h4>product</h4>
+const Product = ({ image, name, price, id }) => {
+  return (
+    <Wrapper>
+      <div className='container'>
+        <img src={image} alt={name} />
+        <Link to={`/products/${id}`} className='link'>
+          <FaSearch />
+        </Link>
+      </div>
+      <footer>
+        <h5>{name}</h5>
+        <p>{formatPrice(price)}</p>
+      </footer>
+    </Wrapper>
+  )
 }
-
 const Wrapper = styled.article`
   .container {
     position: relative;
