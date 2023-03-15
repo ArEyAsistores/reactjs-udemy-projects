191c817f11d402861e90198a2063abec90a1d05a
YONDU\rasistores <rasistores>
2023-03-15 22:20:11 +0800

    project-rohan

--- redux-toolkit-tutorial-main/vite-starter/src/App.jsx
+++ redux-toolkit-tutorial-main/vite-starter/src/App.jsx
@@ -1,22 +1,32 @@
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import CartContainer from "./components/CartContainer";
 import Modal from "./components/modal";
 import Navbar from "./components/navBar";
-import { clearCart } from "./features/cart/cartSlice";
+import { clearCart, getCartItems } from "./features/cart/cartSlice";
 import { closeModal, openModal } from "./features/modal/modalSlice";
 
 function App() {
   const dispatch = useDispatch();
   const { isOpen } = useSelector((state) => state.modal);
- 
+  const { isLoading, cartItems } = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    dispatch(getCartItems());
+  },[])
 
   const handleConfirmed = () => {
     dispatch(clearCart());
     dispatch(closeModal());
   }
-  
+  if(isLoading){
+   return(
+    <div className="loading">
+      <h1>Loading....</h1>
+    </div>
+   );
+  }
   return(
    <main>
     { isOpen && <Modal handleCancel = { () =>  dispatch(closeModal()) } handleConfirmed = { handleConfirmed }/>}
--- redux-toolkit-tutorial-main/vite-starter/src/features/cart/cartSlice.js
+++ redux-toolkit-tutorial-main/vite-starter/src/features/cart/cartSlice.js
@@ -1,5 +1,8 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 import cartItems from  '../../cartItems'
+
+const url = 'https://course-api.com/react-useReducer-cart-project';
 
 const initialState = {
     cartItems: cartItems,
@@ -7,6 +10,23 @@ const initialState = {
     total: 0,
     isLoading: true,
 }
+
+// ** basic setup of fetching API using fetch() 
+// export const getCartItems = createAsyncThunk('cart/getCartItems', () => {
+//     return fetch(url).then((response) => response.json()).catch((error) => console.log(error))
+// });
+//**  fetching API using AXIOS */
+export const getCartItems = createAsyncThunk('cart/getCartItems', async(_,thunkAPI) => {
+    try {
+        // console.log(thunkAPI.getState());
+        const response = await axios(url);
+        return response.data;
+    } catch (error) {
+        return thunkAPI.rejectWithValue('There was an error...');
+    }
+
+});
+
 
 const cartSlice = createSlice({
     name: 'cart',
@@ -45,7 +65,31 @@ const cartSlice = createSlice({
           state.total = totalPrice;
           state.amount = totalCount;
         }
-    }
+    },
+    extraReducers: (builder) => {
+        builder.addCase(getCartItems.pending, (state) => {
+                    state.isLoading = true;
+                })
+                .addCase(getCartItems.fulfilled, (state, action) => {
+                    state.isLoading = false;
+                    state.cartItems = action.payload;
+                }, )
+                .addCase(getCartItems.rejected, (state, action) => {
+                    state.isLoading = false;
+                },)
+    },
+    // extraReducers: {
+    //     [getCartItems.pending]: (state) => {
+    //         state.isLoading = true;
+    //     },
+    //     [getCartItems.fulfilled]: (state, action) => {
+    //         state.isLoading = false;
+    //         state.cartItems = action.payload;
+    //     },
+    //     [getCartItems.rejected]: (state, action) => {
+    //         state.isLoading = false;
+    //     },
+    // }
 })
 
 // console.log(cartSlice);
