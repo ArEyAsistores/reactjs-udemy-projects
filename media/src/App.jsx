7d0947ea2af152619230b9acb68d6dc855c2abc7
YONDU\rasistores <rasistores>
2023-03-16 16:21:16 +0800

    media-project

---
+++ media/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import UserList from './components/UserList'
+
+function App() {
+
+  return (
+    <div className="container mx-auto">
+      <UserList/>
+    </div>
+  )
+}
+
+export default App
---
+++ media/src/store/slices/usersSlice.jsx
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const usersSlice = createSlice({
+
+    name:'users',
+    initialState: {
+        data: []
+    },
+    reducers: {}
+});
+
+
+export const usersReducer = usersSlice.reducer;
